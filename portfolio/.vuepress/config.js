3cbdd4906e32550ee891b794997c688eb369527e
Michael Holroyd <meekohi@gmail.com>
2018-08-16 13:05:04 -0400

    update settings

--- portfolio/.vuepress/config.js
+++ portfolio/.vuepress/config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  title: "Acme",
-  description: "creative agency",
+  title: "Meekohi",
+  description: "techtrek",
   base: "/",
   themeConfig: {
     logo: "/upload/logo.svg",
-    footer: "Ⓒ Acme",
+    footer: "Ⓒ Meekohi",
     nav: [
       { text: "Works", link: "/", position: "left", external: false },
-      { text: "Instagram", link: "https://www.instagram.com/its.nwa/", position: "left", external: true },
-      { text: "Say hi!", link: "mailto:sayhi@mydomain.com", position: "right", external: true },
+      { text: "Instagram", link: "https://www.instagram.com/meekohi/", position: "left", external: true },
+      { text: "Say hi!", link: "mailto:meekohi@gmail.com", position: "right", external: true },
       { text: 'Journal', link: '/journal/', position: 'right', external: false },
     ]
   },
